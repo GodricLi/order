0197e6ab3890cbef67ea4752358bea62fec2f741
GodricLi <635434705@qq.com>
2019-10-15 22:52:15 +0800

    update a member editing function

--- web/controllers/member/Members.py
+++ web/controllers/member/Members.py
@@ -3,6 +3,8 @@ from flask import Blueprint, request, jsonify, redirect
 from common.libs.Helper import ops_render, pagination
 from common.models.member.Member import Member
 from application import app, db
+from common.libs.UrlManager import UrlManager
+from common.libs.Helper import get_current_time
 
 route_member = Blueprint('member_page', __name__)
 
@@ -40,15 +42,88 @@ def index():
 
 @route_member.route("/info")
 def info():
+    res = {}
+    req_data = request.values
+    member_id = int(req_data.get('id', 0))
+    back_url = UrlManager.buildUrl('/member/index')
+    if member_id < 1:
+        return redirect(back_url)
+    member_info = Member.query.filter_by(id=member_id).first()
+    if not member_info:
+        return redirect(back_url)
+    res['info'] = member_info
+    res['current'] = 'index'
+    return ops_render("member/info.html", res)
 
-    return ops_render("member/info.html")
 
-
-@route_member.route("/set")
-def set():
-    return ops_render("member/set.html")
+@route_member.route("/set", methods=['GET', 'POST'])
+def member_set():
+    if request.method == 'GET':
+        res = {}
+        req_data = request.args
+        member_id = int(req_data.get('id', 0))
+        back_url = UrlManager.buildUrl('/member/index')
+        if member_id < 1:
+            return redirect(back_url)
+        member_info = Member.query.filter_by(id=member_id).first()
+        if not member_info:
+            return redirect(back_url)
+        if member_info.status != 1:
+            return redirect(back_url)
+        res['info'] = member_info
+        res['current'] = 'index'
+        return ops_render("member/set.html", res)
+    res = {'code': 200, 'msg': '操作成功', 'data': {}}
+    req_data = request.values
+    member_id = req_data['id'] if 'id' in req_data else 0
+    nickname = req_data['nickname'] if 'nickname' in req_data else ''
+    if not nickname or len(nickname) < 1:
+        res['code'] = -1
+        res['msg'] = '用户名不规范'
+        return jsonify(res)
+    member_info = Member.query.filter_by(id=member_id).first()
+    if not member_info:
+        res['code'] = -1
+        res['msg'] = '指定会员不存在'
+        return jsonify(res)
+    member_info.nickname = nickname
+    member_info.update_time = get_current_time()
+    db.session.add(member_info)
+    db.session.commit()
+    return jsonify(res)
 
 
 @route_member.route("/comment")
 def comment():
     return ops_render("member/comment.html")
+
+
+@route_member.route('/ops', methods=['GET', 'POST'])
+def ops():
+    res = {'code': 200, 'msg': '操作成功', 'data': {}}
+    req_data = request.values
+    member_id = req_data['id'] if 'id' in req_data else 0
+    act = req_data['act'] if 'act' in req_data else ''
+
+    if not member_id:
+        res['code'] = -1
+        res['msg'] = '请选择账号'
+        return jsonify(res)
+    if act not in ['remove', 'recover']:
+        res['code'] = -1
+        res['msg'] = '操作有误'
+        return jsonify(res)
+    member_info = Member.query.filter_by(id=member_id).first()
+    if not member_info:
+        res['code'] = -1
+        res['msg'] = '指定会员不存在'
+        return jsonify(res)
+    if act == 'remove':
+        member_info.status = 0
+    elif act == 'recover':
+        member_info.status = 1
+
+    member_info.update_time = get_current_time()
+    db.session.add(member_info)
+    db.session.commit()
+    return jsonify(res)
