15327707f4cbda17a94a89783a16be6e301dc799
GodricLi <635434705@qq.com>
2019-11-20 20:18:59 +0800

    update

--- web/controllers/api/My.py
+++ web/controllers/api/My.py
@@ -2,3 +2,10 @@
 # @Author　 : Ric
 
 from web.controllers.api import route_api
+from flask import request, jsonify, g
+
+
+@route_api.route("/my/order")
+def my_order_list():
+    res = {'code': 200, 'msg': '操作成功~', 'data': {}}
+    return jsonify(res)
