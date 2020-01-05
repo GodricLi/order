6c3bcf02997c40a18886d1da1a0da166f39d16aa
GodricLi <635434705@qq.com>
2020-01-05 14:37:23 +0800

    update

--- web/controllers/api/My.py
+++ web/controllers/api/My.py
@@ -51,5 +51,6 @@ def my_order_list():
         for item in pay_order_list:
             tmp_data = {
                 'status': item.pay_status,
+                'status_desc': item.status_desc,
 
     return jsonify(res)
